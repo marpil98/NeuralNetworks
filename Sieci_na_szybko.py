4e98b9eebd26fb40d124e80251945784abb584a6
marpil98 <mar.pilewski98@gmail.com>
2024-10-02 11:21:28 +0200

    Dodanie batchnormalizacji dla lstm

--- Sieci_na_szybko.py
+++ Sieci_na_szybko.py
@@ -1,21 +1,22 @@
 
 from tensorflow.keras.layers import LSTM,Dense,Conv2D,Flatten
 # Funkcje do stworzenia różnych rodzajów sieci "na szybko" ograniczone do kilku najważniejszych parametrów 
-def siec_rekurencyjna(x,warstwy,neurony,aktywacja_LSTM,aktywacja_rekurencji,out_act = 'linear'):
+def siec_rekurencyjna(x,warstwy,wyjscia,aktywacja_LSTM,aktywacja_rekurencji,drop,l2_ratio,out_act = 'linear'):
 
-    if type(neurony)==int:
-        neurony = [neurony for _ in range(warstwy)]
+    if type(wyjscia)==int:
+        wyjscia = [wyjscia for _ in range(warstwy)]
     if type(aktywacja_LSTM)==str:
         aktywacja_LSTM = [aktywacja_LSTM for _ in range(warstwy)]
     if type(aktywacja_rekurencji)==str:
         aktywacja_rekurencji = [aktywacja_rekurencji for i in range(warstwy)]
     
     for i in range(warstwy):
+        x=BatchNormalization()(x)
         if i == warstwy-1:
-            x = LSTM(units=neurony[i],activation=aktywacja_LSTM[i],recurrent_activation=aktywacja_rekurencji[i],return_sequences=False)(x)
+            x = LSTM(units=wyjscia[i],activation=aktywacja_LSTM[i],recurrent_activation=aktywacja_rekurencji[i],return_sequences=False,dropout=drop,kernel_regularizer = l2(l2_ratio))(x)
         else:
             print(aktywacja_LSTM[i])
-            x = LSTM(units=neurony[i],activation=aktywacja_LSTM[i],recurrent_activation=aktywacja_rekurencji[i],return_sequences=True)(x)
+            x = LSTM(units=wyjscia[i],activation=aktywacja_LSTM[i],recurrent_activation=aktywacja_rekurencji[i],return_sequences=True,dropout=drop,kernel_regularizer = l2(l2_ratio))(x)
     out = Dense(units = 1,activation = out_act)(x)
     
     return out
